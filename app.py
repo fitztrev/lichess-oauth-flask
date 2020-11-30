cd56606305f994d15b709b97a61c8ede09b6e3d5
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2020-11-29 21:34:54 -0500

    change host

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def authorize():
     return jsonify(**response.json())
 
 if __name__ == '__main__':
-    app.run(port = int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port = int(os.environ.get("PORT", 5000)))
